f672467570ec98e549f73f8c308cdd12760f646c
Joshua Godalle <joshuagodalle210904@gmail.com>
2025-07-04 15:48:46 +0800

    Heroes Images Api

--- r8-backend/routes/api.php
+++ r8-backend/routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameMatchController;
 use App\Http\Controllers\MatchTeamController;
+use App\Http\Controllers\Api\HeroController;
 
 Route::middleware('api')->group(function () {
     Route::apiResource('matches', GameMatchController::class);
     
 Route::apiResource('match-teams', MatchTeamController::class);
+Route::get('/heroes', [HeroController::class, 'index']);
 });
